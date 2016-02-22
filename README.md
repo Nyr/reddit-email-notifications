29d91d1a198af4bbdbc220b04c54503d60b73ee9
Nyr <me@nyr.be>
2016-02-22 01:57:50 +0100

    Switch from RSS to JSON
    reddit switched from RSS 2.0 to Atom 1.0, breaking this script.
    
    Script rewritten to use JSON, XMLStarlet isn’t needed any longer.

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 ##reddit-email-notifications
 Get notifications about new reddit messages by email.
 
-###Requeriments
-Unix based system with `xmlstarlet` available. Just get it from your package manager.
-
 ###Installation
-1. `apt-get install xmlstarlet` (or whatever you use on your system)
-2. Edit `MAIL` `FROMMAIL` and `RSS_URL` on the script and add it to your cron tab so it runs periodically.
+Just edit `MAIL` `FROMMAIL` and `JSON_URL` in the script and add it to your cron tab so it runs periodically.
 
 ###Known limitations
-- The `-a` flag of mail works on Debian but it doesn't in Mac OS X so you may need to remove it.
+The `-a` flag of mail works on Debian but it doesn't in Mac OS X so you may need to remove it.
--- reddit-email-notifications.sh
+++ reddit-email-notifications.sh
@@ -4,24 +4,17 @@
 MAIL="you@example.com"
 # FROMMAIL = mail which will appear in the "from" header of the notifications
 FROMMAIL="noreply@example.com"
-# Get your private inbox feed here: https://www.reddit.com/prefs/feeds/
-RSS_URL="https://www.reddit.com/message/inbox/.rss?feed=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&user=XXXXX"
+# Get the URL for your inbox in JSON here: https://www.reddit.com/prefs/feeds/
+JSON_URL="https://www.reddit.com/message/inbox/.json?feed=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&user=XXXXX"
 
 # No need to edit from here :)
 BASEDIR=$(dirname $0)
 mkdir $BASEDIR/output 2>/dev/null
 NEWITEMS="$BASEDIR/output/newitems.txt"
 SENTITEMS="$BASEDIR/output/sentitems.txt"
-MAILTMP="$BASEDIR/output/mail.tmp"
 
-# Get the feed, remove crap and the empty line at the end
-wget -qO- -T 15 -t 3 --no-check-certificate $RSS_URL | xmlstarlet sel -t -m //channel/item -v "link" -o " %#%#%#%#%" -v "title" -n | \
-sed 's/ via .*//' | sed 's/ sent.*//' | sed 's/%#%#%#%#%.*from //g' | sed '/^$/d'> $NEWITEMS
-
-# Check if feed download was successful
-if ! egrep -q -i "www.reddit.com/r/|www.reddit.com/message/messages/" $NEWITEMS ; then
-	exit
-fi
+# Download the feed, get the content we need
+wget -qO- -T 15 -t 3 --no-check-certificate $JSON_URL | sed 's/", "/\n/g' | grep -E 'author": "|id": "' | grep -v 'parent_id' | sed 's/id": "//g' | sed 's/subreddit": "//g' | sed 's/author": "//g' | paste -d " " - - > $NEWITEMS
 
 # If it's the first time, we don't want to spam our inbox
 if [ ! -f $SENTITEMS ]; then
@@ -31,16 +24,15 @@ fi
 
 # Extract the variables we need and send the emails
 while IFS= read -r LINE; do
-	IFS=' ' read url user < <(echo "$LINE")
-	if ! grep -q $url $SENTITEMS; then # Only if the URL wasn't sent already...
+	IFS=' ' read author id < <(echo "$LINE")
+	if ! grep -q $id $SENTITEMS; then # Only if the URL wasn't sent already...
 		# Build the email and send it
-		echo "Hey," > $MAILTMP
-		echo >> $MAILTMP
-		echo "$user has sent you a message:" >> $MAILTMP
-		echo >> $MAILTMP
-		echo "$url" >> $MAILTMP
-		mail -a "From: $FROMMAIL" -s "Message from $user on reddit" $MAIL < $MAILTMP
+		echo "Hey,
+
+$author has sent you a message:
+
+https://www.reddit.com/message/inbox/" | mail -a "From: $FROMMAIL" -s "Message from $author on reddit" $MAIL
 		# Add it to the log
-		echo $url $user >> $SENTITEMS
+		echo $author $id >> $SENTITEMS
 	fi
 done < $NEWITEMS
